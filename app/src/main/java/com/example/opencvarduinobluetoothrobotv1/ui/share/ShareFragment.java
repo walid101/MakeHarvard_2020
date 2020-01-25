f22dfedc5e46924b2a6918dd69a7a8af02b000e0
walid101 <nuhankhan101@gmail.com>
2020-01-25 11:37:57 -0500

    Got rid of bugs that cause crashes

--- app/src/main/java/com/example/opencvarduinobluetoothrobotv1/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/opencvarduinobluetoothrobotv1/ui/share/ShareFragment.java
@@ -1,5 +1,6 @@
 package com.example.opencvarduinobluetoothrobotv1.ui.share;
 
+import android.app.ActionBar;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -47,11 +49,13 @@ import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
-public class ShareFragment extends Fragment implements View.OnClickListener
-{
+public class ShareFragment extends Fragment implements View.OnClickListener {
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
     BluetoothAdapter bluetoothAdapter;
     private String mConnectedDeviceName = null;
-
+    BluetoothAdapter mBluetoothAdapter = null;
     /**
      * Array adapter for the conversation thread
      */
@@ -61,55 +65,83 @@ public class ShareFragment extends Fragment implements View.OnClickListener
      * String buffer for outgoing messages
      */
     private StringBuffer mOutStringBuffer;
-
+    private TextView textView = null; //used to be final
     private BluetoothChatService mChatService = null;
     private ShareViewModel shareViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         shareViewModel = ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.blueOps);
+        textView = root.findViewById(R.id.blueOps);
         shareViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
-            Button onOffButton = root.findViewById(R.id.BlueOn);
-            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-            onOffButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    enableDisableBT();
-                    if (!bluetoothAdapter.isEnabled()) {
-
-                        Snackbar snackBar = Snackbar.make(textView.findViewById(R.id.blueOps),
-                                R.string.turned_on, Snackbar.LENGTH_LONG);
-                        snackBar.show();
-
-
-                    } else if (bluetoothAdapter.isEnabled()) {
-
-                        Snackbar snackBar = Snackbar.make(textView.findViewById(R.id.blueOps),
-                                R.string.turned_off, Snackbar.LENGTH_LONG);
-                        snackBar.show();
-
-
-                    }
-                }
-            });
-            Button blueTest = root.findViewById(R.id.BlueTest);
-            blueTest.setOnClickListener(new View.OnClickListener() {
+        Button onOffButton = root.findViewById(R.id.BlueOn);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = bluetoothAdapter;
+        onOffButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendMessage("1");
+                enableDisableBT();
+                if (!bluetoothAdapter.isEnabled()) {
+
+                    Snackbar snackBar = Snackbar.make(textView.findViewById(R.id.blueOps),
+                            R.string.turned_on, Snackbar.LENGTH_LONG);
+                    snackBar.show();
+
+
+                } else if (bluetoothAdapter.isEnabled()) {
+
+                    Snackbar snackBar = Snackbar.make(textView.findViewById(R.id.blueOps),
+                            R.string.turned_off, Snackbar.LENGTH_LONG);
+                    snackBar.show();
+
+
+                }
+            }
+        });
+        Button blueTest = root.findViewById(R.id.BlueTest);
+        blueTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    sendMessage("1");
+                } catch (Exception e) {
+                    Snackbar snac = Snackbar.make(textView.findViewById(R.id.blueOps), "BlueTooth Chat Service is null (most likely), not paired",
+                            Snackbar.LENGTH_LONG);
+                    snac.show();
+                }
             }
         });
         //The following code is a test to see if the system can communicate via blutooth currently
 
 
         return root;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            setupChat();
+        }
+
+
+    }
+
+    public void setupChat()
+    {
+        mChatService = new BluetoothChatService(getActivity(), mHandler);
     }
     public void enableDisableBT()
     {
@@ -142,7 +174,10 @@ public class ShareFragment extends Fragment implements View.OnClickListener
     private void sendMessage(String message) {
         // Check that we're actually connected before trying anything
         if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
-            Toast.makeText(getActivity(), R.string.not_connected, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(getActivity(), R.string.not_connected, Toast.LENGTH_SHORT).show();
+            Snackbar snac = Snackbar.make(textView.findViewById(R.id.blueOps),"NOT CONNECTED TO BLUTOOTH",
+                    Snackbar.LENGTH_LONG);
+            snac.show();
             return;
         }
 
@@ -156,5 +191,82 @@ public class ShareFragment extends Fragment implements View.OnClickListener
             //mOutStringBuffer.setLength(0);
 
         }
+    }
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            FragmentActivity activity = getActivity();
+            switch (msg.what) {
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            setStatus(getString(R.string.title_connected_to, mConnectedDeviceName));
+                            mConversationArrayAdapter.clear();
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            setStatus(R.string.title_connecting);
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            setStatus(R.string.title_not_connected);
+                            break;
+                    }
+                    break;
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                    if (null != activity) {
+                        /*
+                        Toast.makeText(activity, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                                */
+
+                    }
+                    break;
+                case Constants.MESSAGE_TOAST:
+                    if (null != activity) {
+                        /*
+                        Toast.makeText(activity, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                                */
+
+                    }
+                    break;
+            }
+        }
+    };
+    private void setStatus(int resId) {
+        FragmentActivity activity = getActivity();
+        if (null == activity) {
+            return;
+        }
+        final ActionBar actionBar = activity.getActionBar();
+        if (null == actionBar) {
+            return;
+        }
+        actionBar.setSubtitle(resId);
+    }
+    private void setStatus(CharSequence subTitle) {
+        FragmentActivity activity = getActivity();
+        if (null == activity) {
+            return;
+        }
+        final ActionBar actionBar = activity.getActionBar();
+        if (null == actionBar) {
+            return;
+        }
+        actionBar.setSubtitle(subTitle);
     }
 }
