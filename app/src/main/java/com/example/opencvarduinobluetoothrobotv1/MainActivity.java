f2b1a5a8bd71493bdadd33f57e029248f50c9b3f
FrancisGS131 <syfrancis131@gmail.com>
2020-01-25 00:08:04 -0500

    Testing commit

--- app/src/main/java/com/example/opencvarduinobluetoothrobotv1/MainActivity.java
+++ app/src/main/java/com/example/opencvarduinobluetoothrobotv1/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //Testing commit FRANCIS!
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
